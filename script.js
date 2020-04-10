dcdd906b2e518909967bf2655e0a382fd927733d
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-10 12:04:16 -0700

    Init classes

---
+++ script.js
@@ -0,0 +1,15 @@
+//Normal calculator class
+
+class StandardCalculator {
+    constructor(...args) {
+        this.args = [...args]
+    }
+}
+
+//Science calc 
+
+class ScienceCalculator {
+    constructor(...args) {
+        this.args = [...args]
+    }
+}
