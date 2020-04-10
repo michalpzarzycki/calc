71e3598e518c0ec8803f4ef6de5f819cf4554612
Michal Zarzycki <michal.p.zarzycki@wp.pl>
2020-04-10 12:52:52 -0700

    Added modules

---
+++ ScienceCalculator.mjs
@@ -0,0 +1,18 @@
+//Science calc 
+
+export default class ScienceCalculator {
+    constructor(...args) {
+        this.args = [...args]
+    }
+
+       //standard multiply
+   //standard divide
+   //standard substract
+   //standard add
+
+   //sinus
+   //cosinus
+   //tangens
+   //ctg
+
+}
--- script.mjs
+++ script.mjs
@@ -1,22 +1,14 @@
+import ScienceCalculator from './ScienceCalculator.mjs'
+
 //DOM ELEMENTS
 const INPUT_1 = document.getElementById("1");
 const INPUT_2 = document.getElementById("2");
 const CALCULATION_BUTTONS = document.querySelectorAll('input[type="button"]')
-// EVENT LISTENERS
-CALCULATION_BUTTONS[0].addEventListener('click', handleCalculation )
 
-// Calculator
-class Calculator extends StandardCalculator{
-    constructor(...args) {
-        this.args = [...args]
-    }
 
-    //check method
-    //add
-    //substract
-    //multiply
-    //add
-}
+console.log("HEJ")
+
+
 
 
 //Normal calculator class
@@ -32,31 +24,27 @@ class StandardCalculator extends ScienceCalculator {
    //standard add
 }
 
-//Science calc 
-
-class ScienceCalculator {
+// Calculator
+class Calculator extends StandardCalculator{
     constructor(...args) {
         this.args = [...args]
     }
 
-       //standard multiply
-   //standard divide
-   //standard substract
-   //standard add
 
-   //sinus
-   //cosinus
-   //tangens
-   //ctg
-
+    //check method
+    //add
+    //substract
+    //multiply
+    //add
 }
 
-//handle calculation
 function handleCalculation(event) {
     event.preventDefault();
-    console.log(INPUT_1.value, INPUT_2.value)
+    console.log("WORK")
 }
-
 //handle standard calculation
 
 //handle Science calculation
+
+// EVENT LISTENERS
+CALCULATION_BUTTONS[0].addEventListener('click', handleCalculation)
